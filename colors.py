49e79744023b4580ed0065fcb1d75cc9ff75f454
Brendan <brendan.c.byrne2.civ@mail.mil>
2016-01-15 12:55:26 -0500

    reordered colors to be similar to matplotlib

--- colors.py
+++ colors.py
@@ -5,27 +5,27 @@
 to matplotlibs default colors
 '''
 
-dark = ['#ee2e2f',
+dark = ['#185aa9',
+        '#ee2e2f',
         '#008c48',
-        '#185aa9',
         '#f47d23',
         '#662c91',
         '#a21d21',
         '#b43894',
         '#010202']
 
-medium = ['#f15a60',
+medium = ['#5a9bd4',
+          '#f15a60',
           '#7ac36a',
-          '#5a9bd4',
           '#faa75b',
           '#9e67ab',
           '#ce7058',
           '#d77fb4',
           '#737373']
 
-light = ['#f2afad',
+light = ['#b8d2ec',
+         '#f2afad',
          '#d9e4aa',
-         '#b8d2ec',
          '#f3d1b0',
          '#d5b2d4',
          '#ddb9a9',
